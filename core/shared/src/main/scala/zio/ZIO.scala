3338835979a587a8a2d4013e53f91c0a25902768
asr2003 <162500856+asr2003@users.noreply.github.com>
2024-11-19 11:27:08 +0530

    add review suggestion
    Co-authored-by: kyri-petrou <67301607+kyri-petrou@users.noreply.github.com>

--- core/shared/src/main/scala/zio/ZIO.scala
+++ core/shared/src/main/scala/zio/ZIO.scala
@@ -1068,7 +1068,7 @@ sealed trait ZIO[-R, +E, +A]
     onExit {
       case Exit.Success(value) => success(value)
       case Exit.Failure(cause) => error(cause)
-    }.catchAll(_ => ZIO.unit).as(())
+    }.ignore
 
   /**
    * Runs the specified effect if this effect fails, providing the error to the
