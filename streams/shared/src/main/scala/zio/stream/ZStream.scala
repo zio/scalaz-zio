d05023fe8312c1b4c63d4f7fd93b7a314743b37f
Jules Ivanic <jules.ivanic@gmail.com>
2024-12-12 23:04:43 +1100

    Clean `zio.stream.ZStream#toPull` code

--- streams/shared/src/main/scala/zio/stream/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/ZStream.scala
@@ -3489,8 +3489,8 @@ final class ZStream[-R, +E, +A] private (val channel: ZChannel[R, Any, Any, Any,
     channel.toPull.map { pull =>
       pull.foldZIO(
         success = {
-          case Left(done)  => Exit.failNone
-          case Right(elem) => Exit.succeed(elem)
+          case Right(elem)            => Exit.succeed(elem)
+          case _: Left[Any, Chunk[A]] => Exit.failNone
         },
         failure = error => Exit.fail(Some(error))
       )
