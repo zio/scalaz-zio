5af08797def4038d9381188db01b85265c501266
Jules Ivanic <jules.ivanic@gmail.com>
2024-12-15 12:34:44 +1100

    fmt

--- stacktracer/src/main/scala/zio/internal/stacktracer/TracerUtils.scala
+++ stacktracer/src/main/scala/zio/internal/stacktracer/TracerUtils.scala
@@ -5,9 +5,9 @@ object TracerUtils {
   /**
    * Parse the trace string into location, file and line
    *
-   * Implementation note:
-   * It parses the string from the end to the beginning for performances reasons
-   * See [[zio.internal.stacktracer.Tracer.createTrace]] for the format of the trace
+   * Implementation note: It parses the string from the end to the beginning for
+   * performances reasons. See [[zio.internal.stacktracer.Tracer.createTrace]]
+   * for the format of the trace
    */
   def parse(trace: String): Option[(String, String, Int)] = {
     val length = trace.length
