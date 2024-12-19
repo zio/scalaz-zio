624f9e050a51cd38fabf5d9f718b9bc7dacbd61a
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2024-12-19 10:10:22 +0100

    Avoid double scanning of the line number part of the trace string + add missing unit tests

--- stacktracer/src/main/scala/zio/internal/stacktracer/TracerUtils.scala
+++ stacktracer/src/main/scala/zio/internal/stacktracer/TracerUtils.scala
@@ -15,44 +15,39 @@ private[internal] object TracerUtils {
    *   successfully parsed trace or null if the trace is invalid
    */
   def parse(trace: String): ParsedTrace = {
-    // Checking the closing parentesis
+    // Checking the closing parentesis starting from the end of the trace
     val closingParentesisIdx = trace.length - 1
     if (closingParentesisIdx < 0 || trace.charAt(closingParentesisIdx) != ')') null
     else {
-      // Finding the colon
+      // Parsing the line number down to the colon
       var colonIdx = closingParentesisIdx - 1
-      while (colonIdx >= 0 && trace.charAt(colonIdx) != ':') {
+      var digitWeight      = 1
+      var line     = 0
+      var ch: Char = 0
+      while (colonIdx >= 0 && {
+        ch = trace.charAt(colonIdx)
+        ch != ':'
+      }) {
+        if (
+          ch < '0' || ch > '9' || (digitWeight == 1000000000 && ch > '2') || {
+            line += (ch - '0') * digitWeight
+            digitWeight *= 10
+            line < 0
+          }
+        ) return null
         colonIdx -= 1
       }
-      if (colonIdx < 0) null
-      else {
-        // Finding the opening parentesis
-        var openingParentesisIdx = colonIdx - 1
-        while (openingParentesisIdx >= 0 && trace.charAt(openingParentesisIdx) != '(') {
-          openingParentesisIdx -= 1
-        }
-        if (openingParentesisIdx < 0) null
-        else {
-          // Parsing the line number
-          var line     = 0
-          var digitIdx = colonIdx + 1
-          while (digitIdx < closingParentesisIdx) {
-            val ch = trace.charAt(digitIdx)
-            digitIdx += 1
-            if (
-              ch < '0' || ch > '9' || line > 214748364 || {
-                line = line * 10 + (ch - '0')
-                line <= 0
-              }
-            ) return null
-          }
-          ParsedTrace(
-            location = trace.substring(0, openingParentesisIdx),
-            file = trace.substring(openingParentesisIdx + 1, colonIdx),
-            line = line
-          )
-        }
+      // Finding the opening parentesis
+      var openingParentesisIdx = colonIdx - 1
+      while (openingParentesisIdx >= 0 && trace.charAt(openingParentesisIdx) != '(') {
+        openingParentesisIdx -= 1
       }
+      if (line == 0 || openingParentesisIdx < 0) null
+      else ParsedTrace(
+        location = trace.substring(0, openingParentesisIdx),
+        file = trace.substring(openingParentesisIdx + 1, colonIdx),
+        line = line
+      )
     }
   }
 }
