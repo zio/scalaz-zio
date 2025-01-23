478f2401fd296974dc9d54bf2435871c0b5001cc
Kyri Petrou <kyri.petrou@outlook.com>
2025-01-23 07:09:08 +0200

    Make compat methods package-private

--- core/shared/src/main/scala-3/zio/ZIOCompanionVersionSpecific.scala
+++ core/shared/src/main/scala-3/zio/ZIOCompanionVersionSpecific.scala
@@ -210,12 +210,12 @@ private[zio] transparent trait ZIOCompanionVersionSpecific {
 
   @targetName("succeed")
   @deprecated("use succeed", "2.1.7")
-  def _succeedCompat[A](a: Unsafe ?=> A)(implicit trace: Trace): ZIO[Any, Nothing, A] =
+  private[zio] def _succeedCompat[A](a: Unsafe ?=> A)(implicit trace: Trace): ZIO[Any, Nothing, A] =
     succeed(a)
 
   @targetName("succeedBlocking")
   @deprecated("use succeedBlocking", "2.1.7")
-  def _succeedBlockingCompat[A](a: Unsafe ?=> A)(implicit trace: Trace): UIO[A] =
+  private[zio] def _succeedBlockingCompat[A](a: Unsafe ?=> A)(implicit trace: Trace): UIO[A] =
     ZIO.blocking(ZIO.succeed(a))
 
 }
