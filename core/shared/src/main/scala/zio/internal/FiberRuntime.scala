9540d13b3621edb8a546f98a983c3b9c2c11538f
Kyri Petrou <kyri.petrou@outlook.com>
2025-02-04 16:14:59 +0200

    Conditionally call Supervisor#onEnd

--- core/shared/src/main/scala/zio/internal/FiberRuntime.scala
+++ core/shared/src/main/scala/zio/internal/FiberRuntime.scala
@@ -424,7 +424,7 @@ final class FiberRuntime[E, A](fiberId: FiberId.Runtime, fiberRefs0: FiberRefs, 
               if (inbox.isEmpty) {
                 finalExit = exit
 
-                supervisor.onEnd(finalExit, self)(Unsafe)
+                if (supervisor ne Supervisor.none) supervisor.onEnd(finalExit, self)(Unsafe)
 
                 // No more messages to process, so we will allow the fiber to end life:
                 self.setExitValue(exit)
